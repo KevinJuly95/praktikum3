e48e912fddd00cb913e1cc12c55848b11a484e12
KevinJuly <32257783+KevinJuly95@users.noreply.github.com>
2018-04-08 16:20:52 +0800

    Instance, buat atribut dan panggil method

--- src/praktikum3/keretaAksi.java
+++ src/praktikum3/keretaAksi.java
@@ -11,6 +11,14 @@ package praktikum3;
  */
 public class keretaAksi {
     public static void main(String[] args) {
+        kereta s = new kereta ();
+        s.jenis="KRCHitachi";
+        s.kursi=80;
+        s.gerbang=8;
+        s.kecepatan=120;
+        s.awak="28";
+        
+        s.cetakInfo();
         
     }
 }

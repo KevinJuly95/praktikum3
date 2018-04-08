be374423cb5dd76968b04256e0b52e16c91da6c1
KevinJuly <32257783+KevinJuly95@users.noreply.github.com>
2018-04-08 16:58:40 +0800

    Getter and Setter

--- src/praktikum3/keretaapilokomotif.java
+++ src/praktikum3/keretaapilokomotif.java
@@ -10,5 +10,20 @@ package praktikum3;
  * @author ACER
  */
 public class keretaapilokomotif extends kereta{
+    private String jenisKelas;
+    
+    void throttle() {
+        System.out.println("KERETA KHUSUS WIBU. . .");
+        
+    }
+
+    public String getJenisKelas() {
+        return jenisKelas;
+    }
+
+    public void setJenisKelas(String jenisKelas) {
+        this.jenisKelas = jenisKelas;
+    }
+    
     
 }

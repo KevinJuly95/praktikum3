5eae183c5761b9c62a2c67a5043c43fedeba593d
KevinJuly <32257783+KevinJuly95@users.noreply.github.com>
2018-04-08 16:26:49 +0800

    buat Getter and Setter

--- src/praktikum3/kereta.java
+++ src/praktikum3/kereta.java
@@ -22,5 +22,49 @@ public class kereta {
                 "gerbang \t: "+gerbang+"\n"+
                 "kecepatan \t: "+kecepatan+" km/j"+"\n"+
                 "awak \t: "+awak+"\n");
+        
+        
     }
+
+    public String getJenis() {
+        return jenis;
+    }
+
+    public void setJenis(String jenis) {
+        this.jenis = jenis;
+    }
+
+    public int getKursi() {
+        return kursi;
+    }
+
+    public void setKursi(int kursi) {
+        this.kursi = kursi;
+    }
+
+    public int getGerbang() {
+        return gerbang;
+    }
+
+    public void setGerbang(int gerbang) {
+        this.gerbang = gerbang;
+    }
+
+    public int getKecepatan() {
+        return kecepatan;
+    }
+
+    public void setKecepatan(int kecepatan) {
+        this.kecepatan = kecepatan;
+    }
+
+    public String getAwak() {
+        return awak;
+    }
+
+    public void setAwak(String awak) {
+        this.awak = awak;
+    }
+    
+    
 }
